6410811531a94e5adaa97a139869c5705bf32059
Niall Gaffney <ngaffney@tacc.utexas.edu>
2018-12-06 16:37:07 -0600

    Fixed version and popup box anoyances

--- json-schema-viewer/lib/example.js
+++ json-schema-viewer/lib/example.js
@@ -4,16 +4,17 @@
     var loc = window.location,
         //if not already set, set the root schema location
         //this allows dev ENV to override the schema location
-        schema = JSV.schema ? JSV.schema : loc.origin + loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1) + 'schemas/schema/schema.json';
+        schema = JSV.schema
 
     JSV.init({
       schema : schema
     }, function() {
       //initialize welcome popup
-      var $popup = $('#popup-welcome').enhanceWithin().popup();
+      //var $popup = $('#popup-welcome').enhanceWithin().popup();
 
       //display schema version
-      JSV.setVersion(tv4.getSchema(JSV.treeData.schema).version);
+      //  commented out as it does nothing but throw exceptions
+      //JSV.setVersion(tv4.getSchema(JSV.treeData.schema).version);
       //handle permalink
       if (window.jsvInitPath) {
         var node = JSV.expandNodePath(window.jsvInitPath.split('-'));
@@ -23,7 +24,7 @@
       } else {
         JSV.resetViewer();
         //show popup
-        $popup.popup('open');
+        //$popup.popup('open');
       }
     });
   });
